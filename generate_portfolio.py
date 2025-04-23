609478e18c68a1d9f64cbf650ac30f545f3d97f5
gpspence <105173090+gpspence@users.noreply.github.com>
2025-04-23 17:57:25 +0100

    Add Jinja templating Python code

--- generate_portfolio.py
+++ generate_portfolio.py
@@ -1,6 +1,19 @@
 import json
-from datetime import UTC, datetime
 from pathlib import Path
 
 from jinja2 import Environment, FileSystemLoader
 
+# Load JSON portfolio data
+with Path("portfolio.json").open(encoding="utf-8") as f:
+    data = json.load(f)
+
+# Set up Jinja environment
+env = Environment(loader=FileSystemLoader("."), autoescape=True)
+index_template = env.get_template("index_template.html")
+
+# Render the template with the data
+html_output = index_template.render(**data)
+
+# Write the output to an HTML file
+with Path("index.html").open("w", encoding="utf-8") as f:
+    f.write(html_output)
